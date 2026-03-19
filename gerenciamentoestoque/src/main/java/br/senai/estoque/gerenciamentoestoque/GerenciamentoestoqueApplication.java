5a2f993082a26d9c40249e805114f27f18c17457
spidemyer <emilymima.jessica@gmail.com>
2026-03-19 08:04:40 -0300

    agora vai

---
+++ gerenciamentoestoque/src/main/java/br/senai/estoque/gerenciamentoestoque/GerenciamentoestoqueApplication.java
@@ -0,0 +1,13 @@
+package br.senai.estoque.gerenciamentoestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoestoqueApplication.class, args);
+	}
+
+}
